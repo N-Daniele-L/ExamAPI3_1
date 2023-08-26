0afeacbd8ef87b8382a0d6b2a5cfed2968f43cfc
N-Daniele-L <daniele.nicolo@condorcet.be>
2023-08-26 11:48:41 +0200

    Oubli des interfaces spéciales

---
+++ src/mvp/model/SpecialBureau.java
@@ -0,0 +1,9 @@
+package mvp.model;
+
+import metier.Bureau;
+
+import java.util.List;
+
+public interface SpecialBureau {
+    List<Bureau> NbrEmployeInDep();
+}
---
+++ src/mvp/model/SpecialEmploye.java
@@ -0,0 +1,8 @@
+package mvp.model;
+
+import metier.Employe;
+
+public interface SpecialEmploye {
+
+    Employe readAdresseEmploye(String adresse);
+}
